1878a6c2618cad84a9d6fe5daa80ebdf571adb7e
Robert <robert.breuer@htwg-konstanz.de>
2023-02-12 18:21:09 +0100

    Added some vulnerabilities for catrgory Likely Bug (Behavioral Problems) from SARD

---
+++ 3_Likely-Bug/CWE-438/CWE-484_71-v1.0.0/src/main/java/Omitted_break_statement.java
@@ -0,0 +1,18 @@
+{
+int month = 8;
+switch (month) {
+case 1: print("January");
+case 2: print("February");
+case 3: print("March");
+case 4: print("April");
+case 5: println("May");
+case 6: print("June");
+case 7: print("July");
+case 8: print("August");
+case 9: print("September");
+case 10: print("October");
+case 11: print("November");
+case 12: print("December");
+}
+println(" is a great month");
+}
